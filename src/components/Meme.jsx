14902ba3261b5b30e84d0c51744c4e54cf1fcdb6
m-moharrami <moharram.m98@gmail.com>
2022-07-16 18:18:57 +0430

    made main(meme) part of the page

---
+++ src/components/Meme.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Meme() {
+    return(
+        <main>
+            <form className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button className="form--bottom">Get a new image 🖼</button>
+            </form>
+        </main>
+    )
+}
