732044eafb09e38bba4d5dcf9e541427b310106c
m-moharrami <moharram.m98@gmail.com>
2022-07-16 21:25:35 +0430

    refactor state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import memesData from '../memesData'
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: ""
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
 
     return(
@@ -25,7 +34,7 @@ export default function Meme() {
                 />
                 <button className="form--bottom" onClick={getMemeImage}>Get a new image 🖼</button>
             </div>
-            <img src={memeImage} className="meme--image" />
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
